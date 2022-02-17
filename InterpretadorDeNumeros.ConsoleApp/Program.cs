f2b43d51d973ab42446bed81be709be3069285b8
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-17 16:13:35 -0300

    entradas lidas

--- InterpretadorDeNumeros.ConsoleApp/Program.cs
+++ InterpretadorDeNumeros.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace InterpretadorDeNumeros.ConsoleApp
 {
@@ -6,7 +7,27 @@ namespace InterpretadorDeNumeros.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            string numeros = @"
+                     __  __        __   __  __   __   __   __ 
+                  |  __| __| |__| |__  |__    | |__| |__| |  |
+                  | |__  __|    |  __| |__|   | |__|  __| |__|
+            ";
+
+            string entradas = @"
+                  __   __ __   __   __      __  __
+                 |__  |__   | |__| |__|   | __| __||__|  
+                  __| |__|  | |__|  __|   ||__  __|    |      
+            ";
+
+            StringReader linhas = new StringReader(entradas);
+            linhas.ReadLine();
+            string linha1 = linhas.ReadLine();
+            Console.WriteLine("Linha 1: "+linha1);
+            string linha2 = linhas.ReadLine();
+            Console.WriteLine("Linha 2: " + linha2);
+            string linha3 = linhas.ReadLine();
+            Console.WriteLine("Linha 3: " + linha3);
+
         }
     }
 }
