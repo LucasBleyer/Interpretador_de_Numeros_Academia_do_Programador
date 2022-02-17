9b115898e12b3bb4c63fd0c8e9156149a1635921
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-17 17:18:13 -0300

    todas as entradas corrigidas

--- InterpretadorDeNumeros.ConsoleApp/Program.cs
+++ InterpretadorDeNumeros.ConsoleApp/Program.cs
@@ -7,26 +7,64 @@ namespace InterpretadorDeNumeros.ConsoleApp
     {
         static void Main(string[] args)
         {
-            string numeros = @"
-                     __  __        __   __  __   __   __   __ 
-                  |  __| __| |__| |__  |__    | |__| |__| |  |
-                  | |__  __|    |  __| |__|   | |__|  __| |__|
-            ";
-
             string entradas = @"
-                  __   __ __   __   __      __  __
-                 |__  |__   | |__| |__|   | __| __||__|  
-                  __| |__|  | |__|  __|   ||__  __|    |      
+             __  __ __   __  __      __  __     
+            |__ |__   | |__||__|   | __| __||__|
+             __||__|  | |__| __|   ||__  __|   |
+
+             __      __  __      __  __  __  __ 
+             __||__|   ||__|   | __||__ |__ |__|
+             __|   |   ||__|   ||__  __||__| __|
+
+             __  __  __  __  __       __  __    
+             __| __||__ |__ |__| |__|   ||__|  |
+             __||__  __||__| __|    |   ||__|  |
+
+             __  __  __  __  __  __      __     
+            |  | __||__ |__    ||__|   ||__||__|
+            |__||__  __||__|   ||__|   |   |   |
             ";
 
             StringReader linhas = new StringReader(entradas);
             linhas.ReadLine();
+
+            Console.WriteLine("Entrada:");
+
             string linha1 = linhas.ReadLine();
-            Console.WriteLine("Linha 1: "+linha1);
+            Console.WriteLine(linha1);
             string linha2 = linhas.ReadLine();
-            Console.WriteLine("Linha 2: " + linha2);
+            Console.WriteLine(linha2);
             string linha3 = linhas.ReadLine();
-            Console.WriteLine("Linha 3: " + linha3);
+            Console.WriteLine(linha3);
+            linhas.ReadLine();
+
+            string linha4 = linhas.ReadLine();
+            Console.WriteLine(linha4);
+            string linha5 = linhas.ReadLine();
+            Console.WriteLine(linha5);
+            string linha6 = linhas.ReadLine();
+            Console.WriteLine(linha6);
+            linhas.ReadLine();
+
+            string linha7 = linhas.ReadLine();
+            Console.WriteLine(linha7);
+            string linha8 = linhas.ReadLine();
+            Console.WriteLine(linha8);
+            string linha9 = linhas.ReadLine();
+            Console.WriteLine(linha9);
+            linhas.ReadLine();
+
+            string linha10 = linhas.ReadLine();
+            Console.WriteLine(linha10);
+            string linha11 = linhas.ReadLine();
+            Console.WriteLine(linha11);
+            string linha12 = linhas.ReadLine();
+            Console.WriteLine(linha12);
+            linhas.ReadLine();
+
+            Console.WriteLine("\r\n------------------------------------------------------------");
+
+            Console.WriteLine("\r\nSaída:");
 
         }
     }
