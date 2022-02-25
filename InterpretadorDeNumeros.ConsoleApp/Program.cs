84636b3f62c25b6049aa3775b7149e5a4dffa65b
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-25 15:16:31 -0300

    organização de métodos

--- InterpretadorDeNumeros.ConsoleApp/Program.cs
+++ InterpretadorDeNumeros.ConsoleApp/Program.cs
@@ -17,30 +17,83 @@ namespace InterpretadorDeNumeros.ConsoleApp
             linhas.ReadLine();
 
             Console.WriteLine("Entrada:");
-            string linha1, linha2, linha3, linha4;
+            string linha1, linha2, linha3;
 
-            LeitorDeTodasAsLinhas(linhas, out linha1, out linha2, out linha3, out linha4);
+            LeitorDeTodasAsLinhas(linhas, out linha1, out linha2, out linha3);
 
-            ConverteTodasAsLinhasArrayChar(linha1, linha2, linha3, linha4);
+            char[] l1 = ConverteLinhasEmArrayChar(linha1, linha2, linha3);
 
-            Console.WriteLine("\r\n------------------------------------------------------------");
+            Console.WriteLine("\r\n------------------------------------------------------");
             Console.WriteLine("\r\nSaída:");
 
-            String conj_caracteres1 = " ", conj_caracteres2 = " ", conj_caracteres3 = " ";
+            String conj_caracteres1 = "", conj_caracteres2 = "", conj_caracteres3 = "";
 
-            for (int i = 0; i < linha1.Length; i++)
+            for (int i = 0; i < l1.Length; i++)
             {
                 conj_caracteres1 += linha1[i];
                 conj_caracteres2 += linha2[i];
-                conj_caracteres3 += linha2[i];
+                conj_caracteres3 += linha3[i];
 
-                
+                if (conj_caracteres1 == " __ " && conj_caracteres2 == "|  |" && conj_caracteres3 == "|__|")
+                {
+                    Console.Write("0");
+                    ResetarConjuntos(out conj_caracteres1, out conj_caracteres2, out conj_caracteres3);
+                }
+
+                if (conj_caracteres1 == "    " && conj_caracteres2 == "   |" && conj_caracteres3 == "   |")
+                {
+                    Console.Write("1");
+                    ResetarConjuntos(out conj_caracteres1, out conj_caracteres2, out conj_caracteres3);
+                }
+
+                if (conj_caracteres1 == " __ " && conj_caracteres2 == " __|" && conj_caracteres3 == "|__ ")
+                {
+                    Console.Write("2");
+                    ResetarConjuntos(out conj_caracteres1, out conj_caracteres2, out conj_caracteres3);
+                }
+
+                if (conj_caracteres1 == " __ " && conj_caracteres2 == " __|" && conj_caracteres3 == " __|")
+                {
+                    Console.Write("3");
+                    ResetarConjuntos(out conj_caracteres1, out conj_caracteres2, out conj_caracteres3);
+                }
+
+                if (conj_caracteres1 == "    " && conj_caracteres2 == "|__|" && conj_caracteres3 == "   |")
+                {
+                    Console.Write("4");
+                    ResetarConjuntos(out conj_caracteres1, out conj_caracteres2, out conj_caracteres3);
+                }
+                if (conj_caracteres1 == " __ " && conj_caracteres2 == "|__ " && conj_caracteres3 == " __|")
+                {
+                    Console.Write("5");
+                    ResetarConjuntos(out conj_caracteres1, out conj_caracteres2, out conj_caracteres3);
+                }
+                if (conj_caracteres1 == " __ " && conj_caracteres2 == "|__ " && conj_caracteres3 == "|__|")
+                {
+                    Console.Write("6");
+                    ResetarConjuntos(out conj_caracteres1, out conj_caracteres2, out conj_caracteres3);
+                }
+                if (conj_caracteres1 == "__  " && conj_caracteres2 == "  | " && conj_caracteres3 == "  | ")
+                {
+                    Console.Write("7");
+                    ResetarConjuntos(out conj_caracteres1, out conj_caracteres2, out conj_caracteres3);
+                }
+                if (conj_caracteres1 == " __ " && conj_caracteres2 == "|__|" && conj_caracteres3 == "|__|")
+                {
+                    Console.Write("8");
+                    ResetarConjuntos(out conj_caracteres1, out conj_caracteres2, out conj_caracteres3);
+                }
+                if (conj_caracteres1 == " __ " && conj_caracteres2 == "|__|" && conj_caracteres3 == " __|")
+                {
+                    Console.Write("9");
+                    ResetarConjuntos(out conj_caracteres1, out conj_caracteres2, out conj_caracteres3);
+                }
             }
-
+            Console.ReadLine();
         }
-        #region Métodos
 
-        private static void LeitorDeTodasAsLinhas(StringReader linhas, out string linha1, out string linha2, out string linha3, out string linha4)
+        #region Métodos
+        private static void LeitorDeTodasAsLinhas(StringReader linhas, out string linha1, out string linha2, out string linha3)
         {
             linha1 = linhas.ReadLine();
             Console.WriteLine(linha1);
@@ -48,48 +101,21 @@ namespace InterpretadorDeNumeros.ConsoleApp
             Console.WriteLine(linha2);
             linha3 = linhas.ReadLine();
             Console.WriteLine(linha3);
-            linhas.ReadLine();
 
-            linha4 = linhas.ReadLine();
-            Console.WriteLine(linha4);
-            linha5 = linhas.ReadLine();
-            Console.WriteLine(linha5);
-            linha6 = linhas.ReadLine();
-            Console.WriteLine(linha6);
-            linhas.ReadLine();
-
-            linha7 = linhas.ReadLine();
-            Console.WriteLine(linha7);
-            linha8 = linhas.ReadLine();
-            Console.WriteLine(linha8);
-            linha9 = linhas.ReadLine();
-            Console.WriteLine(linha9);
-            linhas.ReadLine();
-
-            linha10 = linhas.ReadLine();
-            Console.WriteLine(linha10);
-            linha11 = linhas.ReadLine();
-            Console.WriteLine(linha11);
-            string linha12 = linhas.ReadLine();
-            Console.WriteLine(linha12);
-            linhas.ReadLine();
         }
-        private static void ConverteTodasAsLinhasArrayChar(string linha1, string linha2, string linha3, string linha4)
+        private static void ResetarConjuntos(out string conj_caracteres1, out string conj_caracteres2, out string conj_caracteres3)
+        {
+            conj_caracteres1 = "";
+            conj_caracteres2 = "";
+            conj_caracteres3 = "";
+        }
+        private static char[] ConverteLinhasEmArrayChar(string linha1, string linha2, string linha3)
         {
             char[] l1 = linha1.ToCharArray();
             char[] l2 = linha2.ToCharArray();
             char[] l3 = linha3.ToCharArray();
-            char[] l4 = linha4.ToCharArray();
-            char[] l5 = linha5.ToCharArray();
-            char[] l6 = linha6.ToCharArray();
-            char[] l7 = linha7.ToCharArray();
-            char[] l8 = linha8.ToCharArray();
-            char[] l9 = linha9.ToCharArray();
-            char[] l10 = linha10.ToCharArray();
-            char[] l11 = linha11.ToCharArray();
-            char[] l12 = linha11.ToCharArray();
+            return l1;
         }
-       
 
         #endregion
     }
